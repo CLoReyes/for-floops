f31f4f32677aabbefb2b04e3cbf5f2af6a34d01f
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-08-27 08:45:49 -0600

    just learning this smh

---
+++ for loops/src/fruit_loops.java
@@ -0,0 +1,39 @@
+
+public class fruit_loops
+	{
+
+		public static void main(String[] args)
+			{
+					challengeOne();
+					challengeTwo();
+					challengeThree();
+					challengefour();
+			}
+
+		private static void challengeOne()
+			{
+				for (int i = 0; i >= 5; i++);
+				System.out.println("I love mullen!");
+				
+			}
+
+		private static void challengeTwo()
+			{
+				
+				
+			}
+		
+		private static void challengeThree()
+			{
+				
+				
+			}
+
+		private static void challengefour()
+			{
+				
+				
+			}
+
+		
+	}
